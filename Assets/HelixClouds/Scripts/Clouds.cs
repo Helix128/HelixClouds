e3f2d0f6497e38d2c23de099a92188d4fb078edd
Helix <diegoatari8@mail.com>
2020-10-19 10:45:28 -0300

    update

--- Assets/HelixClouds/Scripts/Clouds.cs
+++ Assets/HelixClouds/Scripts/Clouds.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 namespace Helix.Rendering
-{
+{   [ExecuteAlways]
     public class Clouds : MonoBehaviour
     {
         Material material;
@@ -11,13 +11,13 @@ namespace Helix.Rendering
         public Vector3 speed = Vector2.one;
         [Tooltip("Samples for the clouds. Less samples will make clouds appear flatter. However, a high sample count may heavily affect performance.")]
         public int samples = 4;
+        public float cloudStepSize = 1;
         [Range(0, 1)]
         public float coverage = 0.5f;
         public Mesh quad;
         public Camera camera;
         public Light sun;
         
-   
         List<Matrix4x4> matrices = new List<Matrix4x4>();
         private void Update()
         {
@@ -35,10 +35,13 @@ namespace Helix.Rendering
             matrices.Clear();
             for (int i = 0; i < samples; i++)
             {
-                Matrix4x4 matrix = Matrix4x4.TRS(pos+(Vector3.up*((float)i-((float)samples/2))*4),Quaternion.Euler(90,0,0),new Vector3(16000,16000,1));
+                Matrix4x4 matrix = Matrix4x4.TRS(pos + (Vector3.up * ((float)i - ((float)samples / 2))*cloudStepSize), Quaternion.Euler(90, 0, 0), new Vector3(2 * camera.farClipPlane, 2 * camera.farClipPlane, 1));
                 matrices.Add(matrix);
             }
-            Graphics.DrawMeshInstanced(quad, 0, material, matrices.ToArray(), samples,null,UnityEngine.Rendering.ShadowCastingMode.Off,false,0,Camera.current);
+        
+           
+                Graphics.DrawMeshInstanced(quad, 0, material, matrices.ToArray(), samples, null, UnityEngine.Rendering.ShadowCastingMode.Off, false, 0, Camera.current);
+            
         }
     }
 }
